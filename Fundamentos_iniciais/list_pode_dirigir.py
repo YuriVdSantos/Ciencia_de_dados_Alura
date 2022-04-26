9e8cedf1dc30b46e1fcf098ac9fc39ec3d1d18ce
Santos <yurivdsantos@outlook.com>
2022-04-26 01:50:40 -0300

    Finalizando 1° dia

---
+++ Fundamentos_iniciais/list_pode_dirigir.py
@@ -0,0 +1,15 @@
+idades = [18,15,25,73,46,25,13,10]
+permissoes =[]
+
+def pode_dirigir():
+    for idade in idades:
+        if idade >= 18:
+            permissoes.append(True)
+            print(f"{idade} Anos! Você pode dirigir!")
+        else:
+            permissoes.append(False)
+            print(f"{idade} Anos! Você não pode dirigir!")
+
+pode_dirigir()
+
+print(idades[-1])
--- Fundamentos_iniciais/pode_dirigir.py
+++ Fundamentos_iniciais/pode_dirigir.py
@@ -1,7 +1,12 @@
+idades = [18,25,13,45,10]
+
 def verf_pode_dirigir():
     idade = int(input("Por favor, digite sua idade: "))
     if int(idade) >= 18:
         print("Você pode dirigir!")
     else:
         print("Você não pode dirigir!")
-verf_pode_dirigir()
+
+
+for idade in idades:
+    verf_pode_dirigir(idades)
